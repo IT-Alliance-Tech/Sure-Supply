a06ec5641558ec558b8dc3342c85179f216a821d
Lawtistic <167568623+Lawtistic@users.noreply.github.com>
2025-08-26 18:57:50 +0530

    updates

--- Desktop/comride_admin/src/components/chat-box/header-chat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HeaderChat = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default HeaderChat
--- Desktop/comride_admin/src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import rootSaga from "./sagas/rootSaga";
-import createSagaMiddleware from "redux-saga";
-import rootReducer from "./reducers/rootReducer";
-
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(rootSaga);
-export default store;
